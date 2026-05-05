a4d066d81decc0bce1856577cc1c998db50598e1
Jesús Castillo <jcasben02@gmail.com>
2026-05-06 00:46:00 +0200

    Generated open api sources for frontend from specification

---
+++ frontend/src/app/core/api/model/models.ts
@@ -0,0 +1,7 @@
+export * from './authResponse';
+export * from './errorResponse';
+export * from './loginRequest';
+export * from './logoutRequest';
+export * from './refreshRequest';
+export * from './registerRequest';
+export * from './userResponse';
