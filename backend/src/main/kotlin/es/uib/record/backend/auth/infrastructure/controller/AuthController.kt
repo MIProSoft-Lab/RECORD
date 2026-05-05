867ce823a3f0b60bba4e5fd97b8dcfc0ab4765a6
Jesús Castillo <jcasben02@gmail.com>
2026-05-05 23:08:40 +0200

    Implemented new logout endpoint

--- backend/src/main/kotlin/es/uib/record/backend/auth/infrastructure/controller/AuthController.kt
+++ backend/src/main/kotlin/es/uib/record/backend/auth/infrastructure/controller/AuthController.kt
@@ -4,10 +4,12 @@ import es.uib.record.backend.api.AuthApi
 import es.uib.record.backend.auth.application.usecase.LoginUseCase
 import es.uib.record.backend.auth.application.usecase.RefreshUseCase
 import es.uib.record.backend.auth.application.usecase.RegisterUseCase
+import es.uib.record.backend.auth.application.usecase.RevokeTokenUseCase
 import es.uib.record.backend.auth.infrastructure.mapper.toDto
 import es.uib.record.backend.auth.infrastructure.mapper.toResponse
 import es.uib.record.backend.model.AuthResponse
 import es.uib.record.backend.model.LoginRequest
+import es.uib.record.backend.model.LogoutRequest
 import es.uib.record.backend.model.RefreshRequest
 import es.uib.record.backend.model.RegisterRequest
 import org.springframework.http.ResponseEntity
@@ -17,12 +19,18 @@ import org.springframework.web.bind.annotation.RestController
 class AuthController(
     private val registerUseCase: RegisterUseCase,
     private val loginUseCase: LoginUseCase,
-    private val refreshUseCase: RefreshUseCase
+    private val refreshUseCase: RefreshUseCase,
+    private val revokeTokenUseCase: RevokeTokenUseCase
 ) : AuthApi {
 
     override fun login(loginRequest: LoginRequest): ResponseEntity<AuthResponse> {
         val authResponse: AuthResponse = loginUseCase.execute(loginRequest.toDto()).toResponse()
         return ResponseEntity.ok(authResponse)
+    }
+
+    override fun logout(logoutRequest: LogoutRequest): ResponseEntity<Unit> {
+        this.revokeTokenUseCase.execute(logoutRequest.token)
+        return ResponseEntity.ok().build()
     }
 
     override fun register(registerRequest: RegisterRequest): ResponseEntity<AuthResponse> {
