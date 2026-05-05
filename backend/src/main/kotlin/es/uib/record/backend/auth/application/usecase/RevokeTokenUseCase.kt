05dbb9a4fae6bec0ed2e0db9f9031f02b2de8e05
Jesús Castillo <jcasben02@gmail.com>
2026-05-05 23:07:38 +0200

    Implemented new use case to revoke a token when logging out of the app

---
+++ backend/src/main/kotlin/es/uib/record/backend/auth/application/usecase/RevokeTokenUseCase.kt
@@ -0,0 +1,35 @@
+package es.uib.record.backend.auth.application.usecase
+
+import es.uib.record.backend.auth.domain.Token
+import es.uib.record.backend.auth.domain.TokenRepository
+import es.uib.record.backend.auth.domain.exception.InvalidRefreshTokenException
+import es.uib.record.backend.users.open.UserFacade
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.stereotype.Component
+
+@Component
+class RevokeTokenUseCase(
+    private val userFacade: UserFacade,
+    private val tokenRepository: TokenRepository
+) {
+    fun execute(token: String) {
+        val userEmail = SecurityContextHolder.getContext().authentication.name
+        val userId = this.userFacade.getUserIdByEmail(userEmail)
+        val savedToken = this.tokenRepository.findByToken(token)
+            ?: throw InvalidRefreshTokenException()
+
+        if (savedToken.userId != userId) {
+            throw InvalidRefreshTokenException()
+        }
+
+        val revokedToken = Token(
+            savedToken.id,
+            savedToken.userId,
+            savedToken.token,
+            true,
+            savedToken.expired
+        )
+
+        this.tokenRepository.save(revokedToken)
+    }
+}
