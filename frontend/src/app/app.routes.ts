c55774039e98c2b2976c79f21e91b872c27bceb2
Jesús Castillo <jcasben02@gmail.com>
2026-05-06 00:52:20 +0200

    Configured app routes

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,46 @@
+import { Routes } from '@angular/router';
+import { Login } from '@features/auth/pages/login/login';
+import { MainLayout } from '@layouts/main-layout/main-layout';
+import { Dashboard } from '@features/dashboard/dashboard';
+import { guestGuard } from '@core/guards/guest.guard';
+import { authGuard } from '@core/guards/auth.guard';
+import { Register } from '@features/auth/pages/register/register';
+import { Settings } from '@features/settings/settings';
+
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: Login,
+    canActivate: [guestGuard]
+  },
+  {
+    path: 'register',
+    component: Register,
+    canActivate: [guestGuard]
+  },
+  {
+    path: 'settings',
+    component: Settings,
+    canActivate: [authGuard]
+  },
+  {
+    path: '',
+    component: MainLayout,
+    canActivate: [authGuard],
+    children: [
+      {
+        path: 'dashboard',
+        component: Dashboard
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: '/login'
+  }
+];
