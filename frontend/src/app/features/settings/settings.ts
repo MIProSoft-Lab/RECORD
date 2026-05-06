40955fba4b9600f0a03a52b31650de9175dc1651
Jesús Castillo <jcasben02@gmail.com>
2026-05-07 00:15:55 +0200

    Implemented preferences panel where the user can choose the language

--- frontend/src/app/features/settings/settings.ts
+++ frontend/src/app/features/settings/settings.ts
@@ -1,8 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { Card } from 'primeng/card';
+import { Button } from 'primeng/button';
+import { RouterLink } from '@angular/router';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
+import { Select } from 'primeng/select';
+import { FormsModule } from '@angular/forms';
+
+interface Language {
+  name: string;
+  code: string;
+}
 
 @Component({
   selector: 'record-settings',
-  imports: [],
+  imports: [Card, Button, RouterLink, TranslatePipe, Select, FormsModule],
   templateUrl: './settings.html',
 })
-export class Settings {}
+export class Settings {
+  private translate = inject(TranslateService);
+
+  availableLanguages = signal<Language[]>([
+    { name: 'Español', code: 'es' },
+    { name: 'English', code: 'en' },
+    { name: 'Català', code: 'cat' },
+  ]);
+
+  selectedLanguage = signal<Language>(
+    this.availableLanguages().find(
+      (lang) =>
+        lang.code === localStorage.getItem('lang') ||
+        lang.code === this.translate.getCurrentLang(),
+    ) || this.availableLanguages()[0],
+  );
+
+  onLanguageChange(event: any) {
+    const selectedLang: Language = event.value;
+    localStorage.setItem('lang', selectedLang.code);
+    this.translate.use(selectedLang.code);
+  }
+}
