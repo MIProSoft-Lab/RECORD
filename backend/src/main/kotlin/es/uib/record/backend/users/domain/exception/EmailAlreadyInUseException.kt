5dfc9a3bfb645954c8ea1bd53a88422e7cad3bcd
Jesús Castillo <jcasben02@gmail.com>
2026-05-07 00:22:34 +0200

    Added i18n code in the exception

--- backend/src/main/kotlin/es/uib/record/backend/users/domain/exception/EmailAlreadyInUseException.kt
+++ backend/src/main/kotlin/es/uib/record/backend/users/domain/exception/EmailAlreadyInUseException.kt
@@ -5,7 +5,7 @@ import es.uib.record.backend.shared.exception.ErrorType
 
 class EmailAlreadyInUseException(email: String) : DomainException(
     message = "Email $email is already in use.",
-    code = "",
+    code = "EMAIL_ALREADY_IN_USE",
     type = ErrorType.CONFLICT,
     params = mapOf("email" to email)
 )
