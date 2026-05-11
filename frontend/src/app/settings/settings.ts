9ce0cb39b1d593feeb0187095756943b3c2706ee
Jesús Castillo <jcasben02@gmail.com>
2026-05-12 00:04:18 +0200

    chore(repo): remove unused settings scaffolding

--- frontend/src/app/settings/settings.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'record-settings',
-  imports: [],
-  templateUrl: './settings.html',
-  styleUrl: './settings.scss',
-})
-export class Settings {}
